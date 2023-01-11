a9f69db22a8c671c632cf54251ad8cb5e5080617
avinashhedage <avinashh@google.com>
2023-01-11 09:40:25 +0000

    Added android reay_se implementation of KeyMint 300 Applet
    Test: run vts -m VtsAidlKeyMintTarget
    Change-Id: I868b89286761994da3e2ff8a26dfcf0e760315f5

---
+++ ready_se/google/keymint/KM300/Applet/AndroidSEProviderLib/src/com/android/javacard/seprovider/KMDataStoreConstants.java
@@ -0,0 +1,16 @@
+package com.android.javacard.seprovider;
+
+/**
+ * This class holds different interface type constants to differentiate between the instances of
+ * KMComputedKHmacKey, KMAttestationKey, KMDeviceUniqueKeyPair, KMRkpMacKey, and KMAESKey when
+ * passed as generic objects.
+ */
+public class KMDataStoreConstants {
+  // INTERFACE Types
+  public static final byte INTERFACE_TYPE_COMPUTED_HMAC_KEY = 0x01;
+  public static final byte INTERFACE_TYPE_ATTESTATION_KEY = 0x02;
+  public static final byte INTERFACE_TYPE_DEVICE_UNIQUE_KEY_PAIR = 0x03;
+  public static final byte INTERFACE_TYPE_MASTER_KEY = 0x04;
+  public static final byte INTERFACE_TYPE_PRE_SHARED_KEY = 0x05;
+  public static final byte INTERFACE_TYPE_RKP_MAC_KEY = 0x06;
+}
